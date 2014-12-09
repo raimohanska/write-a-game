ce0c2aa70e8e00e9461cc2a0dda6d5a2239b0c23
Juha Paananen <juha.paananen@gmail.com>
2014-12-09 18:39:42 +0200

    add mongo

---
+++ app-database.js
@@ -0,0 +1,45 @@
+var Bacon = require("baconjs")
+var randomstring = require("randomstring")
+
+function AppDatabase(conn, app) {
+  var apps = conn.collection("apps")
+
+  app.get("/apps", function(req, res) {
+    sendResult(mongoFind({}), res)
+  })
+  app.get("/apps/:author", function(req, res) {
+    sendResult(mongoFind({"content.author": req.params.author}), res)
+  })
+  app.get("/apps/:id", function(req, res) {
+    sendResult(mongoFind({"id": req.params.id}).map(".0"), res)
+  })
+  app.get("/apps/:author/:name", function(req, res) {
+    sendResult(mongoFind({"content.author": req.params.author, "content.description": req.params.name}).map(".0"), res)
+  })
+  app.post("/apps", function(req, res) {
+    var data = {
+      id: randomstring.generate(10),
+      content: req.body,
+      date: new Date()
+    }
+    sendResult(mongoPost(data).map(data), res)
+  })
+  function mongoPost(data) {
+    return Bacon.fromNodeCallback(apps, "insert", [data])
+  }
+  function mongoFind(query) {
+    return Bacon.fromNodeCallback(apps.find(query).sort({date: -1}), "toArray")
+  }
+  function sendResult(resultE, res) {
+    resultE.onError(res, "send")
+    resultE.onValue(function(value) {
+      if (value) {
+        res.json(value)
+      } else {
+        res.status(404).send("Not found")
+      }
+    })
+  }
+}
+
+module.exports = AppDatabase
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var mongoUrl = process.env["MONGOHQ_URL"] || "mongodb://localhost/writegame"
+var MongoClient = require('mongodb').MongoClient
+
+var AppDatabase = require("./app-database")
+
+MongoClient.connect(mongoUrl, function(err, conn) {
+  if (err) {
+    console.log("ERROR: cannot connect to mongo at ", mongoUrl, err)
+  } else {
+    AppDatabase(conn, app)
+  }
+})
 
 app.use('/', express.static(__dirname + '/output'))
 app.use('/', express.static(__dirname + '/app'))
